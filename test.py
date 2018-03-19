b3172316050e0421adaf16dbed2877f54c546608
Denver P <23412100+DrTexxOfficial@users.noreply.github.com>
2018-03-19 15:21:44 +1100

    Initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+#import colorama
+#colorama.init()
+#print(colorama.Back.RED + "RED BACK!" + colorama.Style.RESET_ALL)
+#input()
+from colorThis import ct
+
+print("normal text")
+print(ct("red background, yellow text",Back="RED",Fore="YELLOW"))
+print("more normal text")
