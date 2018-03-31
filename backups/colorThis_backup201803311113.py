ee816652caa32d1046399013f036339221e7eb83
Denver P <23412100+DrTexxOfficial@users.noreply.github.com>
2018-03-31 14:59:22 +1100

    various refinements

--- backups/colorThis_backup201803311113.py
+++ backups/colorThis_backup201803311113.py
@@ -1,22 +1,34 @@
+"""
+colorThis by Denver P.
+
+Simplification for Colorama
+"""
+
 # attempt to import colorama, otherwise raise an exception
 try: import colorama # library used for colouring
 except: print("Can't import colorama. Maybe it isn't installed? Maybe you're running 64-bit instead of 32-bit?")
-# try to fix the escape sequences so they work in windows 10 console
-try:
+
+try: # try to fix the escape sequences so they work in windows 10 console
     import os # used to allow win32 console to recognize ANSI/VT100 escape sequences
     os.system('') # stops formatting from appearing as "[41m[33m" and the like
 except: raise Exception("couldn't apply fix for ANSI/VT100 escape sequence recognition under Win32 console in Windows 10")
 
-# define the main function
 def ct(string,**kwargs): # define main function
     ''' EXAMPLE: ct("hello",Back="RED") '''
-    debug=False
+
     if 'autoReset' in kwargs: autoReset = kwargs['autoReset']
     else: autoReset=True
+    
+    if 'debug' in kwargs: debug = kwargs['debug']
+    else: debug=False
+    
     try: # attempt to run function as intended
-        if(debug): print("kwargs:",kwargs)
+        if(debug): print("kwargs:",kwargs) # print all kwards if debugging
+
         tempString = []
-        keywords = ['Back','Fore','Style'] # list the keywords that are recognized for styling by function
+        
+        keywords = ['Back','Fore','Style'] # list of classes from colorama that are recognized by this function
+        
         for keyword in keywords: # for each keyword, do this
             if(debug): print("matching for keyword:",keyword)
             if keyword in kwargs: # if kwarg matches current keyword from list
@@ -33,4 +45,25 @@ def ct(string,**kwargs): # define main function
         if (debug): print("returning a normal string...")
         return(string)
 
-#print(ct("hello",Back="RED"))
+print(ct("hello",Back="RED", debug=True))
+
+"""
+def ctn(*args,**kwargs): # WIP revised version
+    
+    for my_list in args: # for each list encountered
+        
+        if type(my_list) == type(['test','list']):
+            
+            print("it's a list!")
+            
+            for my_item in my_list:
+                
+                if type(my_item) == type('a string'):
+                    
+                    print(my_item,'is a string!')
+                    
+                elif type(my_item) == type(Back='RED'):
+                    
+                    print(my_item,'is a keyword!')
+"""
+
--- test.py
+++ test.py
@@ -1,9 +1,32 @@
 #import colorama
 #colorama.init()
 #print(colorama.Back.RED + "RED BACK!" + colorama.Style.RESET_ALL)
-#input()
-from colorThis import ct
 
-print("normal text")
-print(ct("red background, yellow text",Back="RED",Fore="YELLOW"))
-print("more normal text")
+from colorThis.colorThis import ct
+
+while(True):
+    print()
+    test_to_run = input("Which test would you like to run? [normal,autoReset,showErrors,exit]: ")
+    if(test_to_run=='normal'):
+        print("normal text")
+        print(ct("red back, yellow text! :)",Back="RED",Fore="YELLOW"))
+        print("more normal text")
+    
+    # TESTING AUTORESET FUNCATIONALITY
+    elif(test_to_run=='autoReset'):
+        print(ct("YELLOW FROM HERE",Back='YELLOW',autoReset=False))
+        print("a regular print")
+        print("another regular print")
+        print(ct("I DEMAND NO YELLOW!",Style='RESET_ALL',autoReset=False))
+        print("another another regular print")
+    
+    # TESTING FOR ERRORS
+    elif(test_to_run=='showErrors'):
+        print(ct("DON'T SHOW ME ERRORS!",Back="YELLOW",Fore="RED",StrangerDanger="oh jeez",showErrors=False)) # Ignore errors, just return the string best you can
+        print(ct("TELL ME IF SOMETHING IS WRONG! (DEFAULT)",Back="YELLOW",Fore="RED",StrangerDanger="oh jeez")) # Notify of errors (default behaviour)
+    # EXIT TESTING SUITE
+    elif(test_to_run=='exit'):
+        break
+    else:
+        print("that's not a valid test name")
+    
